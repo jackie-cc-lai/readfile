8ea6912dbce634eeadcd103b25401f778cbdfae7
Jackie <jackielai_123@hotmail.com>
2019-10-29 16:17:51 -0400

    added command line argument and parsing filetype using suffix

--- read_fileinfo.py
+++ read_fileinfo.py
@@ -1,5 +1,9 @@
-import argparse
-import ffmpeg-python
+import sys
+import ffmpeg
 
-if(file.
-stream = ffmpeg.input()
+#Test if file is mov/mp4 or not
+if sys.argv[1].lower().endswith(('.mov', '.mp4')):
+    print("Correct file!")
+	
+else:
+    print("The program is intended for mov and mp4 files only, please try again")
