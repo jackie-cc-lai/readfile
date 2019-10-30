62dbf23507cfa0a4ce80b48aeaf2f9e5543de45d
Jackie <jackielai_123@hotmail.com>
2019-10-30 10:07:16 -0400

    Removed redundant code

--- read_fileinfo.py
+++ read_fileinfo.py
@@ -42,76 +42,68 @@ def getFinalTime():
     logger.info('Duration: ' + dur)
     
 if (file_true):
-    if(sys.argv[1].lower() == '.mp4' or sys.argv[1].lower() == '.mov'):
-        print("The program is intended for mov and mp4 files only, please try again") #Edge case - if the entry is literally just the suffix itself (since thats just not a file)
-        logger.error('Filetype incorrect')
+    if ( not os.path.exists(sys.argv[1]) ): #If the filename is wrong with the right suffix to prevent error when trying to read the file
+        print("File does not exist, please try again")
+        logger.error('File does not exist')
         getFinalTime()
     else:
-        if ( not os.path.exists(sys.argv[1]) ): #If the filename is wrong with the right suffix to prevent error when trying to read the file
-            print("File does not exist, please try again")
-            logger.error('File does not exist')
+        #We now read the metadata via ffmpeg/ffprobe
+        logger.info('Retrieving metadata from file for processing')
+        filemeta = getMetaData(sys.argv[1])
+        if filemeta != '':  
+            #get the hours, min, sec, ms from the duration variable
+            logger.info('Parsing file duration')
+            fileLength = filemeta['streams'][0]['duration']
+            fileLength = float(fileLength)
+            fileHours = int(fileLength / 3600)
+            fileMin = int(fileLength/60 - fileHours*60)
+            fileSec = int(fileLength - fileMin*60 - fileHours*3600)
+            fileMs = int((fileLength - fileSec - fileMin*60 - fileHours*3600)*1000)
+			# Standardize how it looks
+            if fileHours == 0:
+                fileHours = '00'
+            else: 
+                fileHours = str(fileHours)
+            if fileMin == 0:
+                fileMin = '00'
+            else: 
+                fileMin = str(fileMin)
+            if fileSec == 0:
+                fileSec = '00'
+            else: 
+                fileSec = str(fileSec)
+            if fileMs == 0:
+                fileMs = '00'
+            else: 
+                fileMs = str(fileMs)
+             
+            #changing int to string now that the operations are done
+            duration = fileHours+ " hr "+fileMin+" m " + fileSec+" s " + fileMs + " ms"
+            logger.info('Parsing video frames per second')
+            filefps = filemeta['streams'][0]['r_frame_rate']
+            filefps = filefps.split("/")
+            filefps = round(int(filefps[0])/int(filefps[1]))
+            logger.info('parsing file resolution')
+            res = str(filemeta['streams'][0]['width'])+ "px width "+ str(filemeta['streams'][0]['height']) +"px height"
+            bitrate = filemeta['streams'][0]['bit_rate']
+            bitrate = round(int(bitrate)/1000)
+            bitrate = str(bitrate) + "kb/s"
+            #storing into class
+            logger.info('parsing all data into class')
+            fileData = fileMeta(res, duration, filemeta['streams'][0]['codec_type'], bitrate, filemeta['streams'][0]['codec_long_name'], filefps)
+            #writing to csv file
+            logger.info('Writing to csv file')
+            with open('video_info.csv', 'a') as csvWrite:
+                file_write = csv.writer(csvWrite, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+                file_write.writerow( [fileData.res, fileData.duration,fileData.bitrate,fileData.codec,fileData.encoder, fileData.fps])
+                    
             getFinalTime()
         else:
-            #We now read the metadata via ffmpeg/ffprobe
-            logger.info('Retrieving metadata from file for processing')
-            filemeta = getMetaData(sys.argv[1])
-            if filemeta != '':  
-                #get the hours, min, sec, ms from the duration variable
-                logger.info('Parsing file duration')
-                fileLength = filemeta['streams'][0]['duration']
-                fileLength = float(fileLength)
-                fileHours = int(fileLength / 3600)
-                fileMin = int(fileLength/60 - fileHours*60)
-                fileSec = int(fileLength - fileMin*60 - fileHours*3600)
-                fileMs = int((fileLength - fileSec - fileMin*60 - fileHours*3600)*1000)
-				# Standardize how it looks
-                if fileHours == 0:
-                    fileHours = '00'
-                else: 
-                    fileHours = str(fileHours)
-                if fileMin == 0:
-                    fileMin = '00'
-                else: 
-                    fileMin = str(fileMin)
-                if fileSec == 0:
-                    fileSec = '00'
-                else: 
-                    fileSec = str(fileSec)
-                if fileMs == 0:
-                    fileMs = '00'
-                else: 
-                    fileMs = str(fileMs)
-                
-                #changing int to string now that the operations are done
-                duration = fileHours+ " hr "+fileMin+" m " + fileSec+" s " + fileMs + " ms"
-                logger.info('Parsing video frames per second')
-                filefps = filemeta['streams'][0]['r_frame_rate']
-                filefps = filefps.split("/")
-                filefps = round(int(filefps[0])/int(filefps[1]))
-                logger.info('parsing file resolution')
-                res = str(filemeta['streams'][0]['width'])+ "px width "+ str(filemeta['streams'][0]['height']) +"px height"
-                bitrate = filemeta['streams'][0]['bit_rate']
-                bitrate = round(int(bitrate)/1000)
-                bitrate = str(bitrate) + "kb/s"
-                #storing into class
-                logger.info('parsing all data into class')
-                fileData = fileMeta(res, duration, filemeta['streams'][0]['codec_type'], bitrate, filemeta['streams'][0]['codec_long_name'], filefps)
-
-                #writing to csv file
-                logger.info('Writing to csv file')
-                with open('video_info.csv', 'a') as csvWrite:
-                    file_write = csv.writer(csvWrite, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-                    file_write.writerow( [fileData.res, fileData.duration,fileData.bitrate,fileData.codec,fileData.encoder, fileData.fps])
-                    
-                getFinalTime()
-            else:
-                print("File corrupted")
-                logger.error("File corrupted")
-                getFinalTime()
+            print("File corrupted")
+            logger.error("File corrupted")
+            getFinalTime()
             
 else: #file is not of type mov or mp4
     print("The program is intended for mov and mp4 files only, please try again")
     logger.error('Filetype Incorrect')
     getFinalTime()
-
-    
